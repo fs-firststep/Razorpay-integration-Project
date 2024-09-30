1e7925e2906df4eac3c73507d106cf370b849117
Vijay Kumar <vjkr1190@gmail.com>
2024-09-30 14:17:41 +0530

    Razorpay Integration using React and Node.js

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -19,14 +19,14 @@ const Home = () => {
             key,
             amount: order.amount,
             currency: "INR",
-            name: "6 Pack Programmer",
+            name: "DevVijayK",
             description: "Tutorial of RazorPay",
-            image: "https://avatars.githubusercontent.com/u/25058652?v=4",
+            image: "https://drive.google.com/file/d/18x-BocOD9irHtXdUs5yjnaAe9b7eCwT2/view?usp=drive_link",
             order_id: order.id,
             callback_url: "http://localhost:4000/api/paymentverification",
             prefill: {
-                name: "Gaurav Kumar",
-                email: "gaurav.kumar@example.com",
+                name: "Vijay Kumar",
+                email: "vijay.kumar@example.com",
                 contact: "9999999999"
             },
             notes: {
@@ -45,7 +45,7 @@ const Home = () => {
 
             <Stack h={"100vh"} alignItems="center" justifyContent="center" direction={["column", "row"]}>
 
-                <Card amount={5000} img={"https://cdn.shopify.com/s/files/1/1684/4603/products/MacBookPro13_Mid2012_NonRetina_Silver.png"} checkoutHandler={checkoutHandler} />
+                <Card amount={5000} img={"https://images.unsplash.com/photo-1420406676079-b8491f2d07c8?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} checkoutHandler={checkoutHandler} />
                 <Card amount={3000} img={"http://i1.adis.ws/i/canon/eos-r5_front_rf24-105mmf4lisusm_32c26ad194234d42b3cd9e582a21c99b"} checkoutHandler={checkoutHandler} />
 
             </Stack>
